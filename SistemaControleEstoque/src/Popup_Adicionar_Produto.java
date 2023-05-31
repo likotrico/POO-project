22eca35ed09be1d1f3812909e628cf77941f6fdd
murillo <murilloinacio16@gmail.com>
2023-05-31 18:15:14 -0300

    Popup adicionar produto (incompleto)

---
+++ SistemaControleEstoque/src/Popup_Adicionar_Produto.java
@@ -0,0 +1,173 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Popup_Adicionar_Produto {
+    
+    private JFrame frame; //OK
+
+    private JLabel textoAdicionarProduto; //OK
+    private JLabel textoCodigoProduto; //OK
+    private JLabel textoValidadeProduto; //OK
+    private JLabel textoQuantidadeProduto;
+    private JLabel barrinhaCool; //OK
+    private JLabel barrinhaCool2; //OK
+
+    private JTextField inputCodigoProduto; //OK
+    private JTextField inputDiaValidade; //OK
+    private JTextField inputMesValidade; //OK
+    private JTextField inputAnoValidade; //OK
+    private JTextField inputQuantidadeProduto; //OK
+
+    private JButton botaoAdicionar;
+    private JButton botaoCancelar;
+
+    public Popup_Adicionar_Produto(){
+
+        JFrame frame = new JFrame();
+        frame.setLayout(null);
+        frame.setSize(280, 400);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.frame = frame;
+
+        //ADICIONANDO TEXTO ADICIONAR PRODUTO
+        JLabel label1 = new JLabel();
+        label1.setBackground(Color.BLUE);
+        label1.setOpaque(true);
+        Font fonte1 = new Font("Adicionar Produto", Font.BOLD, 20);
+        label1.setFont(fonte1);
+        label1.setText(fonte1.getName());
+        label1.setBounds(50, 0, 180, 30);
+        this.textoAdicionarProduto = label1;
+
+        //ADICIONANDO O TEXTO CÓDIGO PRODUTO
+        JLabel label2 = new JLabel();
+        label2.setBackground(Color.RED);
+        label2.setOpaque(true);
+        Font fonte2 = new Font("Código:", Font.BOLD, 15);
+        label2.setFont(fonte2);
+        label2.setText(fonte2.getName());
+        label2.setBounds(20, 50, 55, 30);
+        this.textoCodigoProduto = label2;
+
+        //ADICIONANDO O INPUT DO CÓDIGO PRODUTO
+        JTextField textfield1 = new JTextField();
+        textfield1.setBounds(120, 50, 90, 30);  
+        this.inputCodigoProduto = textfield1;
+
+        //ADICIONANDO O TEXTO VALIDADE PRODUTO
+        JLabel label3 = new JLabel();
+        label3.setBackground(Color.GREEN);
+        label3.setOpaque(true);
+        Font fonte3 = new Font("Validade:", Font.BOLD, 15);
+        label3.setFont(fonte3);
+        label3.setText(fonte3.getName());
+        label3.setBounds(20, 90, 70, 30);
+        this.textoValidadeProduto = label3;
+
+        //ADICIONANDO INPUT DO DIA VALIDADE
+        JTextField textfield2 = new JTextField();
+        textfield2.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) { 
+                if (textfield2.getText().length() >= 2 )
+                    e.consume(); 
+            }}); 
+            textfield2.setBounds(120, 90, 20, 30);
+            this.inputDiaValidade = textfield2;
+        
+        //ADICIONANDO BARRINHA 
+        JLabel barrinha = new JLabel();
+        barrinha.setBackground(Color.gray);
+        barrinha.setOpaque(true);
+        Font fonteBarrinha = new Font("/", Font.BOLD, 15);
+        barrinha.setFont(fonteBarrinha);
+        barrinha.setText(fonteBarrinha.getName());
+        barrinha.setBounds(140, 90, 5, 30);
+        this.barrinhaCool = barrinha;
+
+        //ADICIONANDO INPUT DO MÊS VALIDADE
+        JTextField textfield3 = new JTextField();
+        textfield3.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) { 
+                if (textfield3.getText().length() >= 2 )
+                    e.consume(); 
+            }}); 
+            textfield3.setBounds(145, 90, 20, 30);
+            this.inputMesValidade = textfield3;
+
+        //ADICIONANDO BARRINHA2
+        JLabel barrinha2 = new JLabel();
+        barrinha2.setBackground(Color.gray);
+        barrinha2.setOpaque(true);
+        barrinha2.setFont(fonteBarrinha);
+        barrinha2.setText(fonteBarrinha.getName());
+        barrinha2.setBounds(165, 90, 5, 30);
+        this.barrinhaCool2 = barrinha2;
+
+        //ADICIONANDO INPUT DO ANO VALIDADE
+        JTextField textfield4 = new JTextField();
+        textfield4.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) { 
+                if (textfield4.getText().length() >= 4 )
+                    e.consume(); 
+            }}); 
+            textfield4.setBounds(170, 90, 40, 30);
+            this.inputAnoValidade = textfield4;
+
+        //ADICIONANDO JLABEL TEXTO QUANTIDADE
+        JLabel label4 = new JLabel();
+        label4.setBackground(Color.YELLOW);
+        label4.setOpaque(true);
+        Font fonte4 = new Font("Quantidade:", Font.BOLD, 15);
+        label4.setFont(fonte4);
+        label4.setText(fonte4.getName());
+        label4.setBounds(20, 130, 90, 30);
+        this.textoQuantidadeProduto = label4;
+
+        //ADICIONANDO INPUT QUANTIDADE PRODUTO
+        JTextField textfield5 = new JTextField();
+        textfield5.setBounds(120, 130, 90, 30);
+        this.inputQuantidadeProduto = textfield5;
+
+        //ADICIONANDO BOTAO ADICIONAR
+        JButton botao1 = new JButton();
+        botao1.setFocusable(false);
+        Font fontebotao1 = new Font("Adicionar", Font.BOLD, 13);
+        botao1.setFont(fontebotao1);
+        botao1.setText(fontebotao1.getName());
+        botao1.setBounds(20, 180, 110, 30);
+        this.botaoAdicionar = botao1;
+        
+        //ADICIONANDO JLABELS
+        this.frame.add(this.textoAdicionarProduto);
+        this.frame.add(this.textoCodigoProduto);
+        this.frame.add(this.textoValidadeProduto);
+        this.frame.add(this.textoQuantidadeProduto);
+
+        this.frame.add(this.barrinhaCool);
+        this.frame.add(this.barrinhaCool2);
+
+        //ADICIONANDO OS TEXTFIELDS
+        this.frame.add(this.inputCodigoProduto);
+        this.frame.add(this.inputDiaValidade);
+        this.frame.add(this.inputMesValidade);
+        this.frame.add(this.inputAnoValidade);
+        this.frame.add(this.inputQuantidadeProduto);
+
+        //ADICIONANDO OS BOTÕES
+        this.frame.add(this.botaoAdicionar);
+
+        this.frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        Popup_Adicionar_Produto pop = new Popup_Adicionar_Produto();
+    }
+    
+}
